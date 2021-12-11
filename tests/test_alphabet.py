fb20a1ecbccbec67e6143e68e266417956d2c440
Arif Akkermans <arif.akkermans@vodafoneziggo.com>
2021-12-11 17:11:44 +0100

    instantiate class and pass an argument

--- tests/test_alphabet.py
+++ tests/test_alphabet.py
@@ -2,25 +2,27 @@ import pytest
 import sure
 from alphabet import LetterFilter
 
-filter = LetterFilter()
+
 string = "onomatopoeia"
 not_asci = "♥O◘♦♥O◘♦"
-
+filter = LetterFilter(string)
 outcome_filter_vowel = "ooaooeia"
 outcome_filter_consontants = "nmtp"
 
 def test_vowel():
-    vowel = filter.filter_vowels(string)
+    vowel = filter.filter_vowels()
     assert vowel.should.equal(outcome_filter_vowel)
 
 def test_consonants():
-    consonants = filter.filter_vowels(string)
+    consonants = filter.filter_vowels()
     assert consonants.should.equal(outcome_filter_vowel)
 
 def test_asci_vowel():
+    filter = LetterFilter(not_asci)
     with pytest.raises(ValueError):
-        faulty_input = filter.filter_vowels(not_asci)
+        faulty_input = filter.filter_vowels()
 
 def test_asci_consonants():
+    filter = LetterFilter(not_asci)
     with pytest.raises(ValueError):
-        faulty_input = filter.filter_vowels(not_asci)
+        faulty_input = filter.filter_vowels()
